e08cc5b3165108afa88e60873505944bc1487bd8
Bhupinder Kumar <bhanusingh045@gmail.com>
2019-04-27 21:42:45 +0000

    Added rotation cipher

--- main.c
+++ main.c
@@ -1,7 +1,72 @@
-#include <stdio.h>
-
-int main ()
+#include<stdio.h>
+#include <stdlib.h>
+int main()
 {
-    printf("hello");
-return 0;
+	char message[100], ch;
+	int i, key;
+	
+	printf("Enter a message to encrypt: ");
+	scanf("%s",&message);
+	printf("Enter key: ");
+	scanf("%d", &key);
+	
+	for(i = 0; message[i] != '\0'; ++i){
+		ch = message[i];
+		
+		if(ch >= 'a' && ch <= 'z'){
+			ch = ch + key;
+			
+			if(ch > 'z'){
+				ch = ch - 'z' + 'a' - 1;
+			}
+			
+			message[i] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){
+			ch = ch + key;
+			
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1;
+			}
+			
+			message[i] = ch;
+		}
+	}
+	
+	
+	printf("Encrypted message: %s\n", message);
+	
+	//char message1[]=message[];
+	printf("Enter a message to decrypt:",message);
+	scanf("%s",&message);
+	printf("Enter key: ");
+	scanf("%d", &key);
+	
+	for(i = 0; message[i] != '\0'; ++i){
+		ch = message[i];
+		
+		if(ch >= 'a' && ch <= 'z'){
+			ch = ch - key;
+			
+			if(ch < 'a'){
+				ch = ch + 'z' - 'a' + 1;
+			}
+			
+			message[i] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){
+			ch = ch - key;
+			
+			if(ch < 'A'){
+				ch = ch + 'Z' - 'A' + 1;
+			}
+			
+			message[i] = ch;
+		}
+	}
+	
+	printf("Decrypted message: %s", message);
+	
+	return 0;
 }
+
